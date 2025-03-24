ef8377899714bdf5d47a18cc9a502e5fedea3bce
Spoo0derman <perseus.10jun2008@gmail.com>
2025-03-24 14:28:35 +0100

    aLot

--- New chatgpt.py
+++ New chatgpt.py
@@ -2,9 +2,14 @@ import pygame
 import random
 import copy
 
+from pygame.examples.moveit import GameObject
+
 pygame.init()
 pygame.mixer.init()
 pygame.joystick.init()
+
+WIDTH, HEIGHT = 900, 800
+display = pygame.display.set_mode([WIDTH, HEIGHT])
 
 if pygame.joystick.get_count() > 0:
     controller = pygame.joystick.Joystick(0)
@@ -16,12 +21,13 @@ else:
 lobby_music = pygame.mixer.Sound("lobby_music.mp3")
 main_music = pygame.mixer.Sound("main_music.mp3")
 
+home_Screen = pygame.image.load("19-playing-cards-png.png").convert_alpha()
+home_Screen = pygame.transform.scale_by(home_Screen, 0.3)
+poker_Chips = pygame.image.load("chips-poker 1.png").convert_alpha()
+poker_Chips = pygame.transform.scale_by(poker_Chips, 0.3)
 lobby_music.play()
 
 pygame.display.set_caption("BLACKJACK - Sam & Sid")
-
-WIDTH, HEIGHT = 900, 800
-display = pygame.display.set_mode([WIDTH, HEIGHT])
 
 Green = (0, 50, 0)
 White = (255, 255, 255)
@@ -29,7 +35,10 @@ Red = (255, 0, 9)
 
 # Game states
 MENU = 0
-GAME = 1
+BETTING = 1
+GAME = 2
+END = 3
+
 state = MENU
 
 # Main loop for the game
@@ -41,7 +50,7 @@ while running:
 
         if event.type == pygame.JOYBUTTONDOWN and controller:
             if event.button == 0:  # "X" button on PS controller (or adjust for Xbox)
-                state = GAME
+                state = BETTING
 
     # Display different screens based on state
     if state == MENU:
@@ -52,7 +61,8 @@ while running:
         text = font.render('BLACKJACK', True, Red)
         display.blit(text, (275, 50))
 
-        pygame.draw.rect(display, (White), (350, 150, 250, 350))
+        #Home Screen Cards image & Button
+        display.blit(home_Screen, (225, 175))
         pygame.draw.rect(display, (127, 127, 127), (300, 600, 350, 100))
 
         small_font = pygame.font.SysFont('Times New Roman', 40)
@@ -63,10 +73,19 @@ while running:
         text_y = 600 + (100 - play_text.get_height()) // 2
         display.blit(play_text, (text_x, text_y))
 
-    elif state == GAME:
+    elif state == BETTING:
         display.fill((0, 100, 0))  # Change background color for the game screen
-        game_text = font.render('Deal me', True, White)
-        display.blit(game_text, (275, 50))
+        game_text = font.render('Bet amount', True, White)
+        display.blit(game_text, (300, 50))
+
+        if controller.get_button(0):  # A for playstation = X / Colour is Red
+            pygame.draw.rect(display, (255, 0, 0), [0, 0, 800, 600])
+        elif controller.get_button(1):  # B for playstation = O / Colour is Green
+            pygame.draw.rect(display, (0, 255, 0), [0, 0, 800, 600])
+        elif controller.get_button(2):  # X for playstation = SQUARE / Colour is Blue
+            pygame.draw.rect(display, (0, 0, 255), [0, 0, 800, 600])
+        elif controller.get_button(3):  # Y for playstation = Triangle / Colour is Orange
+            pygame.draw.rect(display, (255, 165, 0), [0, 0, 800, 600])
 
     pygame.display.flip()
 
