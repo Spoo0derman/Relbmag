afb64acc293004d61aff7b5ed79b88b7e86c3279
kakatootie <samhahn199@gmail.com>
2025-03-26 22:17:29 +0100

    update

--- Game.py
+++ Game.py
@@ -5,7 +5,7 @@ pygame.init()
 
 def set_game(display, Green, poker_Chips, small_font, White, controller, x_button, O_button, square_button, triangle_button, state, GAME):
     display.fill(Green)
-    game_text = small_font.render('The deal', True, White)
-    total_text = small_font.render('deal', True, White)
+    game_text = small_font.render('Bet amount', True, White)
+    total_text = small_font.render('Dealer', True, White)
     display.blit(game_text, (315, 50))
     display.blit(total_text, (200, 250))
